dffbfdceceaf93b9f5bd48ad18742e6074e959fa
Nate <nate.grobe@gmail.com>
2020-05-06 16:15:29 -0400

    exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,6 +10,8 @@ const App = () => {
     const incrementNeutral = newValue => setNeutral(newValue)
     const incrementBad = newValue => setBad(newValue)
 
+    const all = good + neutral + bad
+
     return (
         <div>
             <h2>give feedback</h2>
@@ -23,6 +25,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {(good - bad) / all}</p>
+            <p>positive {good/all*100}%</p>
         </div>
     )
 }
