d4cb754fe3295cba6c00f4c28356b4dab9512b35
Nate <nate.grobe@gmail.com>
2020-05-07 16:25:58 -0400

    exercise 2.13

---
+++ part2/countrydata/src/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import ExpandedCountry from './components/ExpandedCountry'
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(res => {
+                setCountries(res.data)
+            })
+    }, [])
+
+    const handleFilter = (event) => setFilter(event.target.value) 
+    const filteredCountries = countries.filter(country => country.name.toUpperCase().includes(filter.toUpperCase()))
+    
+    return (
+        <div>
+            find countries <input value={filter} onChange={handleFilter} />
+            <p style={{display: filteredCountries.length < 11 ? 'none' : 'block'}}>Too many matches, specify another filter</p>
+
+            <div style={{display: filteredCountries.length < 11 ? 'block' : 'none'}}>
+                {filteredCountries.length !== 1 
+                    ? filteredCountries.map(country => <p key={country.name}>{country.name}</p>)
+                    : filteredCountries.map(country => <ExpandedCountry key={country.name} country={country} />)
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default App
---
+++ part2/countrydata/src/components/ExpandedCountry.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ExpandedCountry = ({country}) => {
+    return (
+        <div>
+            <h2>{country.name}</h2>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+
+            <h3>languages</h3>
+            <ul>
+                {country.languages.map(lang => <li key={lang.name}>{lang.name}</li>)}
+            </ul>
+
+            <img style={{width: '200px', height: '100px'}} src={country.flag} />
+        </div>
+    )
+}
+
+export default ExpandedCountry
