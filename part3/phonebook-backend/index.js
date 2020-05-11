7a453c90b865addadc5f7b62872bf85c596eda5b
Nate <nate.grobe@gmail.com>
2020-05-11 13:50:35 -0400

    exercise 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -36,6 +36,10 @@ app.get('/api/persons/:id', (req, res) => {
     res.json(person)
 })
 
+app.get('/info', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
