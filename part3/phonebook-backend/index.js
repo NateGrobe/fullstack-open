5cb026e28d5720d32f22bcadd82578b66fb37269
Nate <nate.grobe@gmail.com>
2020-05-14 17:54:02 -0400

    removed uneeded code

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -27,35 +27,6 @@ app.use(morgan(':method :url :status :res[content-length] - :total-time ms :json
     }
 }))
 
-/*
-let persons = [
-    {
-        name: 'Arto Hellas',
-        number: '040-123456',
-        id: 1
-    },
-    {
-        name: 'Ada Lovelace',
-        number: '39-44-5323523',
-        id: 2
-    },
-    {
-        name: 'Dan Abramov',
-        number: '12-43-234345',
-        id: 3
-    },
-    {
-        name: 'Mary Poppendieck',
-        number: '39-23-6423122',
-        id: 4
-    },
-]
-*/
-
-const generateId = () => {
-    return Math.floor(Math.random() * 1000)
-}
-
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons.map(person => person.toJSON()))
