05aefc621f4f9c1e4d90cfaf1cdf43f16d0bc785
Nate <nate.grobe@gmail.com>
2020-05-15 13:35:42 -0400

    exercise 3.16

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const Person = require('./models/person')
 
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(cors())
 
 // custom token
@@ -33,10 +33,15 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person.toJSON())
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person)
+                res.json(person.toJSON())
+            else
+                res.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
@@ -53,6 +58,7 @@ app.delete('/api/persons/:id', (req, res) => {
         .then(result => {
             res.status(204).end()
         })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -92,6 +98,22 @@ app.post('/api/persons', (req, res) => {
         res.json(savedNote.toJSON())
     })
 })
+
+// error handling
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknownEndpoint'})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError')
+        return res.status(400).send({error: 'malformatted id'})
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
