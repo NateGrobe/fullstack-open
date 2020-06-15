8ec2a1ee5b774a03f2ed56a3e141583ea39243c0
Nate Grobe <natepgrobe@gmail.com>
2020-06-15 16:54:14 -0400

    exercise 6.20

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
-const AnecdoteForm = () => {
-  const dispatch = useDispatch()
-
+const AnecdoteForm = (props) => {
   const addAnecdote = async event => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    props.createAnecdote(content)
 
-    dispatch(setNotification(`you added '${content}'`, 5))
+    props.setNotification(`you added '${content}'`, 5)
   }
 
   return (
@@ -28,4 +26,12 @@ const AnecdoteForm = () => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AnecdoteForm)
--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { changeFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = (props) => {
   const handleChange = event => {
-    dispatch(changeFilter(event.target.value))
+    props.changeFilter(event.target.value)
   }
 
   const style = {
@@ -19,4 +18,11 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapStateToProps = state => {
+  return { filter: state.filter }
+}
+
+export default connect(
+  mapStateToProps,
+  { changeFilter }
+)(Filter)
