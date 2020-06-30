7bd2a3051be59be791ca87aa5b52e688c9cb8c04
Nate Grobe <natepgrobe@gmail.com>
2020-06-30 16:38:35 -0400

    exercise 8.5

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -95,7 +95,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -105,8 +105,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      return books.filter(book => book.author === args.author)
+      if (!args.author && !args.genre) return books
+      let filteredBooks
+      if(args.author)
+        filteredBooks = books.filter(book => book.author == args.author)
+      if (args.genre) {
+        if(filteredBooks)
+          return filteredBooks.filter(book => book.genres.includes(args.genre))
+        return books.filter(book => book.genres.includes(args.genre))
+      }
     },
     allAuthors: () => authors
   },
