ac506661a67402894861551a776ecfbceb699b1d
Nate Grobe <natepgrobe@gmail.com>
2020-07-10 13:17:46 -0400

    exercise 9.17

---
+++ part9/patientor/patientor-frontent/src/PatientPage/PatientPage.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import { Container, Table, Button, Icon } from "semantic-ui-react";
+
+import { useStateValue } from '../state';
+import { useParams } from 'react-router-dom';
+import { apiBaseUrl } from '../constants';
+import { Patient } from '../types';
+
+
+const PatientPage: React.FC = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string}>();
+  const patient: Patient | undefined = Object.values(patients).find(p => p.id === id);
+
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "UPDATE_PATIENT", payload: patientFromApi});
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if(!patient) fetchPatient();
+  }, [dispatch])
+
+  if (!patient) return null;
+
+  let genderIcon = <Icon name='genderless' />;
+
+  if (patient.gender === 'male')
+    genderIcon = <Icon name='mars' />;
+  else
+    genderIcon = <Icon name='venus' />;
+
+
+
+  return (
+    <div>
+      <h2>{patient.name} {genderIcon}</h2>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientPage;
